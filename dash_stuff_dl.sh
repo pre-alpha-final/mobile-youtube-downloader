c3299393dd38133fcd2b25dfbba953d4050ca9dd
spanishprisoner <jacek.mowinski@gmail.com>
2017-02-14 02:17:47 +0100

    Youtube limits dl speed on low quality download, so adding concurrent dl functionality

--- dash_stuff_dl.sh
+++ dash_stuff_dl.sh
@@ -11,10 +11,15 @@ if [ "$1" == "" ]
 then
  echo ""
  echo SYNOPSIS
- echo " "usage $0 URL
+ echo " "usage $0 url starting_from_x_line every_y_line
  echo ""
  echo DESCRIPTION
  echo "  Downloads audio and video files"
+ echo "  The x and y parameters allow you to do something along this"
+ echo "  $0 url 0 3"
+ echo "  $0 url 1 3"
+ echo "  $0 url 2 3"
+ echo "   from 3 different terminals to have 3 concurrent downloads"
  echo ""
  exit
 fi
@@ -22,7 +27,7 @@ fi
 #
 # Downloading
 #
-LINKS=$(lynx --dump "$1" | awk '/http/{print $2}')
+LINKS=$(lynx --dump "$1" | awk '/http/{print $2}' | sed -n "$2~$3p")
 for link in $LINKS;
 do
   FILENAME=$(ExtractFilename $link)
